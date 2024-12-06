03793927c3e6c9fe28f015af4689a226d20d56bd
Rokibul Alom <itismehey975@gmail.com>
2024-12-06 09:17:32 +0600

    data delete from my equip page is successfully done

--- src/Pages/MyequipmentPage/MyequipmentPage.jsx
+++ src/Pages/MyequipmentPage/MyequipmentPage.jsx
@@ -13,7 +13,21 @@ const MyequipmentPage = () => {
         })
     },[user.email])
     console.log(loaddata);
-    
+    const handleDelete = _id => {
+        console.log('is im getting the id ',_id);
+        fetch(`http://localhost:5000/equipments/${_id}`, {
+            method:"DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            
+            const remaining = loaddata.filter(d => d._id !== _id);
+            setLoaddata(remaining)
+
+            alert('data delted successfully from database')
+        })
+    }
     return (
         <div className="container mx-auto p-6 w-10/12 min-h-screen">
             <h1 className="text-3xl font-bold mb-6 text-center">My Added Equipments</h1>
@@ -38,11 +52,11 @@ const MyequipmentPage = () => {
                                 <td className="border border-gray-300 px-4 py-2">{d.price}</td>
                                 <td className="border border-gray-300 px-4 py-2">
                                     <Link to={`/details/${d._id}`}
-                                        className=" btn bg-green-500 text-white py-1 px-3 rounded"
+                                        className=" btn bg-green-500 text-white mr-3 py-1 px-3 rounded"
                                     >
-                                        View Details
+                                        Update
                                     </Link>
-                                    <Link to={`/details/${d._id}`}
+                                    <Link onClick={()=> handleDelete(d._id)}
                                         className=" btn bg-red-500 text-white py-1 px-3 rounded"
                                     >
                                         Delete
